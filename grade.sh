37b4c0942a302b58595cd073836a37e382e5d8bd
watersis <114371045+watersis@users.noreply.github.com>
2022-11-10 11:50:37 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -1,7 +1,16 @@
 # Create your grading script here
 
-set -e
+#set -e
 
 rm -rf student-submission
 git clone $1 student-submission
-
+cd student-submission
+#CP=" .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar"
+javac -cp .:..lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java
+java -cp .:..lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples.java 2> error.txt >res.txt
+grep -i FAILURE res.txt > error.txt
+ERR=$(wc -w error.txt)
+if [["$ERR" == "0 error.txt"]]
+then echo "grade=100% passed"
+else echo "grade=0% failed"
+fi
