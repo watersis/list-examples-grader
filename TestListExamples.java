ab8755621bd7d771bea90d4227ca5cb902c898bb
watersis <114371045+watersis@users.noreply.github.com>
2022-11-10 11:50:50 -0800

    Update TestListExamples.java

--- TestListExamples.java
+++ TestListExamples.java
@@ -3,4 +3,20 @@ import org.junit.*;
 
 public class TestListExamples {
   // Write your grading tests here!
+  class myStringChecker implements StringChecker{
+    public boolean checkString(String s){
+      if(s.length()>3){
+        return true;
+      }
+      else{
+        return false;
+      }
+    }
+  }
+  public void test1(){
+    List<String> input = Arrays.asList("a","b","c");
+    List<String> input2 = Arrays.asList("d","e");
+     List<String> expect1 = Arrays.asList("a","b","c","d","e");
+    assertEquals(expect1, ListExamples.merge(input1,input2));
+  }
 }
